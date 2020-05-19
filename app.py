f4c0b186fc260245cf67e1808b346fdd54abd1d2
Elliot Hogg <elliot@Elliots-Mac-mini.local>
2020-05-19 21:55:50 +0100

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/signup')
+def sign_up():
+    return render_template('signup.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        if request.form['username'] == 'minamarz' and request.form['password'] == 'kenzo':
+            return redirect(url_for('account'))
+        else:
+            incorrect = "username or password is incorrect"
+            return render_template('login.html', value=incorrect)
+   
+    return render_template('login.html')
+
+    
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
